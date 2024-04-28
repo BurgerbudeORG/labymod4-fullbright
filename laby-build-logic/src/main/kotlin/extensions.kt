bfe75cc0953fdd93391eb6fc6b92d4e7c30b32b1
Christian <r0bby@gmx.net>
2024-04-28 14:49:49 +0200

    1.20.5

--- laby-build-logic/src/main/kotlin/extensions.kt
+++ laby-build-logic/src/main/kotlin/extensions.kt
@@ -1,4 +1,3 @@
-import gradle.kotlin.dsl.accessors._64acc05bf1a66f2c855e386526b4bcff.api
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.artifacts.dsl.DependencyHandler
@@ -11,5 +10,5 @@ fun JavaPluginExtension.javaVersion(javaVersion: JavaVersion) {
 }
 
 fun DependencyHandler.apiProject(rootProject: Project, name: String, configuration: String? = null) {
-  api(project(":${rootProject.name}-$name", configuration))
+  add("api", project(":${rootProject.name}-$name", configuration))
 }
